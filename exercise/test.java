5273169d3d97c160641d69d35d2b9ac603c4ac78
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-21 13:51:26 +0800

    Create test.java

---
+++ exercise/test.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class test{
+    public static void main(String[] args) {
+        int i = 10;
+        for (i = 0; i < 10; i++) {
+            
+        }
+        System.out.println(i);
+    }
+}

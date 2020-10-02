e71055cae3aef2a1e5c250e49eb0fea3e6b9db06
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-02 15:53:09 +0800

    Create Snake.java

---
+++ assignment_1/Snake.java
@@ -0,0 +1,49 @@
+package assignment_1;
+
+public class Snake {
+    public Snake(int width, int height) {
+        // generate the game map of matrix
+
+    }
+
+    public void StartSnake(int i, int j) {
+        // start snake at position (i,j)
+
+    }
+
+    public void ShowSnake() {
+
+        // print the grid with snake
+    }
+
+    public void GenerateFood(int x, int y) {
+
+        // generate food at position(x,y)
+    }
+
+    public void GenerateFoodRandom() {
+
+        // generate food at a random position
+    }
+
+    public void GenerateStone(int x, int y) {
+
+        // generate a stone at position(x,y)
+    }
+
+    public void GenerateStoneRandom() {
+
+    }
+
+    // generate a stone at a random position
+    public boolean FoodOnSnake(int x, int y) {
+
+        // if food is on the snake, return true
+        // food should disappear and generates new food
+    }
+
+    public void Move(int direction) {
+        // move a step according to direction, 0-right, 1-down, 2-left, 3-right
+
+    }
+}

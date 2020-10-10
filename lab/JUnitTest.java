dba37cc25c52de06a2ea7835cef149bff1f86168
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-10 11:58:16 +0800

    Update JUnitTest.java

--- lab/JUnitTest.java
+++ lab/JUnitTest.java
@@ -1,7 +1,7 @@
 package lab;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 
 public class JUnitTest {

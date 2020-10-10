91f3a4ecc7ae512bd1c9aade5d55a895a1bfdc86
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-10 11:58:13 +0800

    Update Maze.java

--- assignment_1/Maze.java
+++ assignment_1/Maze.java
@@ -1,5 +1,7 @@
 package assignment_1;
 
+import java.util.Stack;
+
 public class Maze {
     private int[][] mazeMat;
     private int width;
@@ -11,16 +13,82 @@ public class Maze {
         this.height = height;
     }
 
-    class Node {
-        int x, y;
+    // class Node {
+    // int x, y;
 
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+    // public Node(int x, int y) {
+    // this.x = x;
+    // this.y = y;
+    // }
+    // }
 
+    /**
+     * show the path from start point to end point with value 2 in matrix
+     * 
+     * @param startX
+     * @param startY
+     * @param endX
+     * @param endY
+     */
     public void DFSearchPath(int startX, int startY, int endX, int endY) {
+        Path path = new Path();
+        Path invalid_path = new Path();
+        path.push(new Node(startX, startY));
+
+        int x = startX;
+        int y = startY;
+
+        this.mazeMat[endX][endY] = 0;
+
+        while (!(x == endX && y == endY)) {
+
+            // the first level index increasing indicates downward direction
+            // the second level index increasing indicates rightward direction
+
+            // step right
+            if (y + 1 < width && this.mazeMat[x][y + 1] != 1 && !invalid_path.contains(x, y + 1)
+                    && !path.contains(x, y + 1)) {
+                path.push(new Node(x, y + 1));
+                y++;
+                continue;
+            }
+
+            // step down
+            if (x + 1 < height && this.mazeMat[x + 1][y] != 1 && !invalid_path.contains(x + 1, y)
+                    && !path.contains(x + 1, y)) {
+                path.push(new Node(x + 1, y));
+                x++;
+                continue;
+            }
+
+            // step left
+            if (y - 1 >= 0 && this.mazeMat[x][y - 1] != 1 && !invalid_path.contains(x, y) && !path.contains(x, y - 1)) {
+                path.push(new Node(x - 1, y));
+                y--;
+                continue;
+            }
+
+            // step up
+            if (x - 1 >= 0 && this.mazeMat[x - 1][y] != 1 && !invalid_path.contains(x - 1, y)
+                    && !path.contains(x - 1, y)) {
+                path.push(new Node(x - 1, y));
+                x--;
+                continue;
+            }
+
+            // all the moving directions are invalid, step back
+            Node lastMoveNode = path.pop();
+            x = lastMoveNode.x;
+            y = lastMoveNode.y;
+            invalid_path.push(lastMoveNode);
+        }
+
+        // rewrite the mazeMatrix
+        for (Node step : path) {
+            this.mazeMat[step.x][step.y] = 2;
+        }
+        this.mazeMat[startX][startY] = 3;
+        this.mazeMat[endX][endY] = 4;
     }
 
     @Override
@@ -35,6 +103,43 @@ public class Maze {
         return ResultString;
     }
 
-    public void DFSearchPath(int startX, int startY, int endX, int endY)
-    // show the path from start point to end point with value 2 in matrix.
+}
+
+class Path extends Stack<Node> {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 744463356338360382L;
+
+    public Path() {
+
+    }
+
+    /**
+     * This method is to judge by value whether a pair of coordinate exists in the
+     * path or not
+     * 
+     * @param x the value of x coordinate to be searched
+     * @param y the value of y coordinate to be searched
+     * @return true if exists in the path, false otherwise
+     */
+    public boolean contains(int x, int y) {
+        for (Node node : this) {
+            if (node.x == x && node.y == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
+class Node {
+    int x, y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }

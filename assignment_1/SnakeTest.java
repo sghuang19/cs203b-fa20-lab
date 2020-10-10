9ced87b65d0f3fb97cd94f97d29df13710865e69
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-10 17:02:52 +0800

    Create SnakeTest.java

---
+++ assignment_1/SnakeTest.java
@@ -0,0 +1,30 @@
+package assignment_1;
+
+import java.util.Scanner;
+
+public class SnakeTest {
+    public static void main(String[] args) {
+        // TODO Auto-generated met
+        Snake snake = new Snake(20, 10);
+        snake.StartSnake(3, 3);
+        snake.GenerateFoodRandom();
+        snake.ShowSnake();
+        int i = 0;
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            while (i < 1000) {
+                System.out.println("the snake is going to move to (1:right, 2:down, 3:left)");
+                int dir = sc.nextInt();
+                if (i % (4 * snake.queueSnake.size()) == 0)
+                    snake.GenerateStoneRandom();
+                snake.Move(dir);
+                snake.ShowSnake();
+                i++;
+            }
+        } catch (Error e) {
+            System.out.println("The snake is dead, GAME OVER");
+        }
+        sc.close();
+    }
+}

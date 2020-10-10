7578a99c1eec20d4c47bc83107217564e91e4887
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-10 11:58:19 +0800

    Update helloworld.java

--- lab/helloworld.java
+++ lab/helloworld.java
@@ -4,7 +4,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello world");
+		System.out.print("hello world");
 	}
 	public String print() {
 		return "Hello world";

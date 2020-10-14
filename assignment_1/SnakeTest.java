9992b9b20e39afd8c5b069452931b6ea39172680
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-14 22:12:36 +0800

    Update SnakeTest.java

--- assignment_1/SnakeTest.java
+++ assignment_1/SnakeTest.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SnakeTest {
     public static void main(String[] args) {
         // TODO Auto-generated met
-        Snake snake = new Snake(20, 10);
+        Snake snake = new Snake(4, 4);
         snake.StartSnake(3, 3);
         snake.GenerateFoodRandom();
         snake.ShowSnake();
@@ -14,10 +14,12 @@ public class SnakeTest {
 
         try {
             while (i < 1000) {
-                System.out.println("the snake is going to move to (1:right, 2:down, 3:left)");
+                System.out.println("the snake is going to move to");
+                System.out.println("(1:right, 2:down, 3:left, 4:up)");
                 int dir = sc.nextInt();
                 if (i % (4 * snake.queueSnake.size()) == 0)
                     snake.GenerateStoneRandom();
+                snake.GenerateFoodRandom();
                 snake.Move(dir);
                 snake.ShowSnake();
                 i++;

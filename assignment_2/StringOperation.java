3e8f5282007db6c1e985ebcf26d6a4563d38c257
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-23 22:08:38 +0800

    Create StringOperation.java

---
+++ assignment_2/StringOperation.java
@@ -0,0 +1,38 @@
+package assignment_2;
+
+/**
+ * 给定两个字符串形式的16进制整数 num1 和 num2 。
+ * 
+ * 注意： 1. num1 和 num2 都只包含 0-9A-F，同时可包含+-符号（只考虑出现在首位，也可不出现）；
+ * 
+ * 2. num1 和 num2 均不以零开头，除非是数字 0 本身；
+ * 
+ * 3. 不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式进行处理。
+ * 
+ * 4. 输入的单个字符串长度不超过200位
+ */
+public class StringOperation {
+    /**
+     * 计算它们的和，结果也为字符串形式。
+     * 
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public static String addStrings(String num1, String num2) {
+        /*
+         * TODO: YOUR CODE HERE
+         */
+    }
+
+    /**
+     * 计算乘积，结果也为字符串形式。
+     */
+
+    public static String multiply(String num1, String num2) {
+        /*
+         * TODO: YOUR CODE HERE
+         */
+    }
+
+}

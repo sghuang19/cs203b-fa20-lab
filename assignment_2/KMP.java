40d2bb121e079f3930f1074bc390aa08de450051
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-23 22:08:33 +0800

    Create KMP.java

---
+++ assignment_2/KMP.java
@@ -0,0 +1,50 @@
+package assignment_2;
+
+import java.util.LinkedList;
+
+/**
+ * 通过实现KMP算法，给定一个文本 S 和模式 P ，得出 P 出现在 S 中多少次， 以及出现的位置。
+ */
+public class KMP {
+    /**
+     * 计算出模式 P 的 next 数组。
+     * 
+     * @param pattern
+     * @return the KMP next array of the pattern
+     */
+    public static int[] computeNextArray(String pattern) {
+        int[] next =
+        return new int[] { 0 };
+    }
+
+    /**
+     * 利用第一问实现的next数组 计算 P 出现在 S 中多少次。
+     * 
+     * @param text
+     * @param pattern
+     * @return
+     */
+    public static int KMPSearchTimes(String text, String pattern) {
+        /*
+         * TODO: YOUR CODE HERE 利用第一问实现的next数组
+         */
+        return 0;
+
+    }
+
+    /**
+     * 计算 P 出现在 S 中的位置。利用第一问实现的next数组
+     * 
+     * @param text
+     * @param pattern
+     * @return
+     */
+    public static LinkedList<Integer> KMPFindLocations(String text, String pattern) {
+        /*
+         * TODO: YOUR CODE HERE 利用第一问实现的next数组
+         */
+
+        LinkedList<Integer> a;
+        return a;
+    }
+}

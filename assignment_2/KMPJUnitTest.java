12ed20589bd405aa61853cc78928bbd1b4359cc5
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-24 13:51:44 +0800

    Update KMPJUnitTest.java

--- assignment_2/KMPJUnitTest.java
+++ assignment_2/KMPJUnitTest.java
@@ -14,6 +14,9 @@ public class KMPJUnitTest extends KMP {
 
     public static void main(String[] args) {
 
+        int i = 1;
+        System.out.println(i++);
+        System.out.println(i);
     }
 
     @Before
@@ -21,6 +24,11 @@ public class KMPJUnitTest extends KMP {
     @Test
     public void computeNextArrayTest() {
         int[] next;
+
+        next = computeNextArray("abaabcac");
+        System.out.println(Arrays.toString(next));
+        assertEquals("[-1, 0, 0, 1, 1, 2, 0, 1]", Arrays.toString(next));
+
         next = computeNextArray("touristrealgod");
         System.out.println(Arrays.toString(next));
         assertEquals("[-1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]", Arrays.toString(next));
@@ -38,13 +46,20 @@ public class KMPJUnitTest extends KMP {
         S = "chenljnbwowowoo";
         result = KMPSearchTimes(S, P);
         System.out.println(result);
-        assertEquals("3", result);
+        assertEquals(3, result);
 
         P = "tourist";
         S = "touristrealgod";
         result = KMPSearchTimes(S, P);
         System.out.println(result);
-        assertEquals("1", result);
+        assertEquals(1, result);
+
+        P = "aba";
+        S = "ababa";
+        result = KMPSearchTimes(S, P);
+        System.out.println(result);
+        assertEquals(2, result);
+
     }
 
     @Test
@@ -55,13 +70,13 @@ public class KMPJUnitTest extends KMP {
         S = "chenljnbwowowoo";
         result = KMPFindLocations(S, P);
         System.out.println(result);
-        assertEquals("[8, 10, 12]", result);
+        assertEquals("[8, 10, 12]", result.toString());
 
         P = "tourist";
         S = "touristrealgod";
         result = KMPFindLocations(S, P);
         System.out.println(result);
-        assertEquals("[0]", result);
+        assertEquals("[0]", result.toString());
     }
 
     @After

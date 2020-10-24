030831259cd4e031e516cfc044fe90d3a1c1b7ad
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-24 13:51:51 +0800

    Update test.java

--- exercise/test.java
+++ exercise/test.java
@@ -1,11 +1,13 @@
 package exercise;
 
-public class test{
+public class test {
     public static void main(String[] args) {
         int i = 10;
         for (i = 0; i < 10; i++) {
-            
+
         }
         System.out.println(i);
+        i = 0;
+        System.out.println(i -= 10);
     }
 }

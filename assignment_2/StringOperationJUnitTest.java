7e0b76cc9c6c68b32918f1c802c986eec12673f8
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-24 22:38:17 +0800

    Update StringOperationJUnitTest.java

--- assignment_2/StringOperationJUnitTest.java
+++ assignment_2/StringOperationJUnitTest.java
@@ -6,6 +6,14 @@ import org.junit.Test;
 
 public class StringOperationJUnitTest extends StringOperation {
 
+    public static void main(String[] args) {
+        String num1, num2, result;
+        num1 = "10000";
+        num2 = "F";
+        result = subtractStrings(num1, num2);
+        System.out.println(result);
+    }
+
     @Test
     public void addStringTest() {
         String num1, num2, result;
@@ -41,6 +49,40 @@ public class StringOperationJUnitTest extends StringOperation {
         num2 = "456";
         result = multiply(num1, num2);
         System.out.println(result);
-        assertEquals("56088", result);
+        assertEquals("4EDC2", result);
+
+        num1 = "AA";
+        num2 = "AA";
+        result = multiply(num1, num2);
+        System.out.println(result);
+        assertEquals("70E4", result);
+
+        num1 = "123AAA";
+        num2 = "-4A5B6CD";
+        result = multiply(num1, num2);
+        System.out.println(result);
+        assertEquals("-54B77BDFD622", result);
+    }
+
+    @Test
+    public void subtractStringTest() {
+        String num1, num2, result;
+        num1 = "2";
+        num2 = "3";
+        result = subtractStrings(num1, num2);
+        System.out.println(result);
+        assertEquals("-1", result);
+
+        num1 = "123";
+        num2 = "456";
+        result = subtractStrings(num1, num2);
+        System.out.println(result);
+        assertEquals("-333", result);
+
+        num1 = "AAA123";
+        num2 = "12BAC";
+        result = subtractStrings(num1, num2);
+        System.out.println(result);
+        assertEquals("A97577", result);
     }
 }

6f7eda490b18c0a4c351f51892fec2747c1a636c
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-26 00:13:26 +0800

    Update test.java

--- exercise/test.java
+++ exercise/test.java
@@ -1,7 +1,13 @@
 package exercise;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import exercise.hello;
+
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
+            IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         int i = 10;
         for (i = 0; i < 10; i++) {
 
@@ -16,6 +22,13 @@ public class test {
         int x = Integer.parseInt(hex, 16);
         System.out.println(x);
 
-        System.out.println(23/16);
+        System.out.println(23 / 16);
+
+        // hello.sayHello();
+
+        Class h = Class.forName("exercise.hello");
+        System.out.println(h);
+        Method method = h.getMethod("sayHello");
+        method.invoke(null, null);
     }
 }

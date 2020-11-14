fd1cbab0c2743c1e1b34406c53240e4ab7af2d33
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-14 15:34:29 +0800

    Create TreeNode.java

---
+++ assignment_3/TreeNode.java
@@ -0,0 +1,13 @@
+package assignment_3;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}

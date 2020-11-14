b4039d72b92ecd2654502672d61e7209606c2221
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-14 15:34:24 +0800

    Create BinaryTree.java

---
+++ assignment_3/BinaryTree.java
@@ -0,0 +1,29 @@
+package assignment_3;
+
+public class BinaryTree {
+    public TreeNode root;
+
+    public static boolean AddLeft(TreeNode parent, TreeNode left) {
+    }
+
+    public static boolean AddRight(TreeNode parent, TreeNode right) {
+    }
+
+    public String TraversalInOrder() {
+    }
+
+    public String TraversalPreOrder() {
+    }
+
+    public String TraversalPostOrder() {
+    }
+
+    public String TraversalLevelOrder() {
+    }
+
+    public String PreIn2Post(String preTravesal, String inTravesal) {
+    }
+
+    public String InPost2Pre(String inTravesal, String postTravesal) {
+    }
+}

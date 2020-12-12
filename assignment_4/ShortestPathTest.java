695daee9eee09386fd187ab3d901fa7c26aafa3b
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-12-12 12:12:42 +0800

    Update ShortestPathTest.java

--- assignment_4/ShortestPathTest.java
+++ assignment_4/ShortestPathTest.java
@@ -1,23 +1,13 @@
 package assignment_4;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Stack;
 
 public class ShortestPathTest {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
 
         File file = new File("assignment_4\\numbs.txt");
-        Graph graph = new Graph(file);
+        // Graph graph = new Graph(file);
 
     }
 }
